388209479e2db309243a7a81e41cdf6f440a16c0
Afonso Neves <1231819@isep.ipp.pt>
2024-03-12 18:50:34 +0000

    made in newBranch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("This is the first change! Made in main branch");
+        System.out.println("This is the first change in New Branch");
 
     }
 }
